85318dca859b2c2b0fe7757924a13b5b4d912069
mmmries <riesmmm@gmail.com>
2019-05-18 10:28:30 -0600

    handle :ok and :error cases

--- lib/ads1115.ex
+++ lib/ads1115.ex
@@ -2,25 +2,33 @@ defmodule ADS1115 do
   alias Circuits.I2C
   alias ADS1115.Config
 
-  @spec config(reference(), byte()) :: Config.t
+  @config_register <<1>>
+  @sensor_register <<0>>
+
+  @spec config(reference(), byte()) :: {:ok, Config.t} | {:error, term()}
   def config(bus, addr) do
-    {:ok, bytes} = I2C.write_read(bus, addr, <<1>>, 2)
-    Config.decode(bytes)
+    case I2C.write_read(bus, addr, @config_register, 2) do
+      {:ok, bytes} -> {:ok, Config.decode(bytes)}
+      {:error, reason} -> {:error, reason}
+    end
   end
 
-  @spec read(reference(), byte(), Config.comparison, Config.gain) :: integer()
-  def read(bus, addr, comparison, gain \\ 2048) do
+  @spec single_shot_read(reference(), byte(), Config.comparison, Config.gain) :: {:ok, integer()} | {:error, term()}
+  def single_shot_read(bus, addr, comparison, gain \\ 2048) do
     config = %Config{performing_conversion: false, mode: :single_shot, mux: comparison, gain: gain}
-    I2C.write(bus, addr, <<1>> <> Config.encode(config))
-    wait_for_reading(bus, addr)
-    {:ok, <<val::signed-size(16)>>} = I2C.write_read(bus, addr, <<0>>, 2)
-    val
+    data = @config_register <> Config.encode(config)
+    with :ok <- I2C.write(bus, addr, data),
+         :ok <- wait_for_reading(bus, addr),
+         {:ok, <<val::signed-size(16)>>} <- I2C.write_read(bus, addr, @sensor_register, 2)
+         do
+          {:ok, val}
+         end
   end
 
   defp wait_for_reading(bus, addr), do: wait_for_reading(bus, addr, config(bus, addr))
-  defp wait_for_reading(_bus, _addr, %Config{performing_conversion: false}), do: :ok
+  defp wait_for_reading(_bus, _addr, {:error, reason}), do: {:error, reason}
+  defp wait_for_reading(_bus, _addr, {:ok, %Config{performing_conversion: false}}), do: :ok
   defp wait_for_reading(bus, addr, _config) do
-    :timer.sleep(1)
     wait_for_reading(bus, addr, config(bus, addr))
   end
 end
