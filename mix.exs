e7447ce80e99682f69544c61b676c10d7a063db8
mmmries <riesmmm@gmail.com>
2019-05-18 22:22:09 -0600

    first release

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule Ads1115.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
       docs: [
         main: "readme",
         extras: ["README.md"]
@@ -15,19 +16,30 @@ defmodule Ads1115.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:circuits_i2c, "~> 0.3"},
       {:dialyxir, "1.0.0-rc.6", only: :dev},
       {:ex_doc, "~> 0.20", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      description: "Interact with ADS1115 Analog-to-Digital Chips",
+      licenses: ["MIT"],
+      links: %{
+        "Github" => "https://github.com/mmmries/ads1115"
+      },
+      maintainers: [
+        "Michael Ries"
+      ]
     ]
   end
 end
